4790029bbf1e1d566a2344a6970da38fc35ac659
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-09-18 23:40:45 +0900

    add html

--- src/main/java/com/example/myapplication/controller/IndexController.java
+++ src/main/java/com/example/myapplication/controller/IndexController.java
@@ -2,11 +2,12 @@ package com.example.myapplication.controller;
 
 import com.example.myapplication.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
 
@@ -18,7 +19,8 @@ public class IndexController {
     }
 
     @GetMapping
-    public String index() {
-        return service.getMessage();
+    public String index(Model model) {
+        model.addAttribute("message", service.getMessage());
+        return "index";
     }
 }
