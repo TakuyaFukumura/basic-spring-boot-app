16471953b1a07916cbd98efbf2759ed709cdfb9e
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-09-18 23:49:40 +0900

    add Comment

--- src/main/java/com/example/myapplication/controller/IndexController.java
+++ src/main/java/com/example/myapplication/controller/IndexController.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/") // URLとの関連付け http://localhost:8080/ の時に呼ばれる
 public class IndexController {
 
     private final IndexService service;
 
     @Autowired
     public IndexController(IndexService service) {
-        this.service = service;
+        this.service = service; // IndexServiceのインスタンスを使えるようにしている
     }
 
-    @GetMapping
+    @GetMapping // Getされた時の処理 Postは別
     public String index(Model model) {
         model.addAttribute("message", service.getMessage());
         return "index";
