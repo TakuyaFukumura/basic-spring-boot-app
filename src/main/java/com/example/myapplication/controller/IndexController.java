334965fecbb7549432d7975e224aea261545658c
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-09-18 23:07:23 +0900

    add service

--- src/main/java/com/example/myapplication/controller/IndexController.java
+++ src/main/java/com/example/myapplication/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.controller;
 
+import com.example.myapplication.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class IndexController {
 
+    private final IndexService service;
+
+    @Autowired
+    public IndexController(IndexService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public String index() {
-        return "Hello World!";
+        return service.getMessage();
     }
 }
---
+++ src/main/java/com/example/myapplication/service/IndexService.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndexService {
+
+    public String getMessage() {
+        return "Hello World!";
+    }
+}
