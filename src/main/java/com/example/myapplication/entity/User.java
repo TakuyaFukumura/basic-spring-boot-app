ef1dcee022e85096830e3a843114990bcce74621
takuya-fukumura <blackchoco1114@gmail.com>
2025-08-11 13:42:48 +0900

    整形

--- src/main/java/com/example/myapplication/entity/User.java
+++ src/main/java/com/example/myapplication/entity/User.java
@@ -1,14 +1,14 @@
 package com.example.myapplication.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "users")
